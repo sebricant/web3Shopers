1fd421f85c00da9a06b947c0bd23dfccc8d51788
Sebastian Richu Antony <sebricant@gmail.com>
2023-04-20 12:08:52 +0530

    Env update

--- app.js
+++ app.js
@@ -1,20 +1,17 @@
-var createError = require('http-errors');
-const expressLayout= require('express-ejs-layouts')
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var session = require('express-session')
-const multer=require('multer')
-const ConnectMongoDBSession = require('connect-mongodb-session')(session);
-
-
+var createError = require("http-errors");
+const expressLayout = require("express-ejs-layouts");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var session = require("express-session");
+const multer = require("multer");
+const ConnectMongoDBSession = require("connect-mongodb-session")(session);
 
 // CommonJS
 
 // const Swal = require('sweetalert2')
 // const File=require('../model/connection')
-
 
 const multerStorage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -36,65 +33,64 @@ const multerStorage = multer.diskStorage({
 
 const upload = multer({
   storage: multerStorage,
-
 });
 
-
-var userRouter = require('./routes/user');
-var adminRouter = require('./routes/admin');
+var userRouter = require("./routes/user");
+var adminRouter = require("./routes/admin");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.use(expressLayout)
-
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(expressLayout);
 
 // app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use(session({
-  saveUninitialized: false,
-  secret: 'secretKeyIsSecret',
-  resave: false,
-  store: new ConnectMongoDBSession({
-    uri:'mongodb://127.0.0.1:27017/ecommerce',
-    collection: "session"
-  }),
-  cookie: {
-    maxAge: 1000 * 60 * 60 * 24 * 10 // 10 days
-  }
-}));
+app.use(
+  session({
+    saveUninitialized: false,
+    secret: "secretKeyIsSecret",
+    resave: false,
+    store: new ConnectMongoDBSession({
+      uri: process.env.MONGODB_URL,
+      collection: "session",
+    }),
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24 * 10, // 10 days
+    },
+  })
+);
 
+app.use(function (req, res, next) {
+  res.header(
+    "Cache-Control",
+    "no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0"
+  );
+  next();
+});
 
-
-
-app.use(function(req, res, next) { 
-  res.header('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
-   next();
-})
-
-app.use('/', userRouter);
-app.use('/admin', adminRouter);
+app.use("/", userRouter);
+app.use("/admin", adminRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
